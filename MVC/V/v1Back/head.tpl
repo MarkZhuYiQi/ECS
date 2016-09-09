2604c3309ed82ef3dba0277237ea351f72fdce08
MarkZhu <zhuyiqi_026@163.com>
2016-09-09 17:40:51 +0800

    add url rewrite

---
+++ MVC/V/v1Back/head.tpl
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <title>ECS</title>
+    <script src="jsB/jq.js"></script>
+    <link rel="stylesheet" href="cssb/bootstrap/easyui.css">
+    <link rel="stylesheet" href="cssb/icon.css">
+    <script src="jsb/jquery.easyui.min.js"></script>
+</head>
+<body>
--- my.php
+++ my.php
@@ -4,12 +4,17 @@
     define("DB_pass","7777777y");
     define("DB_DSN","mysql:host=localhost;dbname=ecs");
     define("CURRENT_VIEWPATH","v1");    //当前模拟使用的版本文件夹
+    define("CURRENT_VIEWPATH_ADMIN","v1Back");   //后台使用版本文件夹
 
     define("Cache_IP","127.0.0.1");
     define("Cache_Port","11211");       //memcache默认端口
 
+
     define("INCLUDE_PATH","MVC/V/v1/include/"); //include文件存放位置
     define("CACHE_PATH","Cache/"); //include文件存放位置
+
+
+
     define("USER_CRYPEKEY","7777777y"); //用户密码，秘钥
     define("USERLOGIN_CRYPEKEY","MarkZhuLgoin"); //用户登录密码，秘钥
     define("USER_LOGINKEY","user_login");       // 便于用户管理
